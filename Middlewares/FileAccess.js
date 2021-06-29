8d50ca21a0f9d9ce385ae33641efb35afbdcda8b
yasas <manukayasas99@gmail.com>
2021-06-30 02:30:26 +0530

    file access fix

--- Middlewares/FileAccess.js
+++ Middlewares/FileAccess.js
@@ -3,23 +3,28 @@ const { FindFileByName } = require("../Dao/UploadDau");
 const { UserEnum } = require("../Models/UserModel");
 
 // file can be viewed by its author or admin or reviewer
-exports.FileAccess = (req, res, next) => {
+exports.FileAccess = async (req, res, next) => {
   const { name } = req.params;
-  FindFileByName(name)
-    .then((upload) => {
-      if (upload.user !== "public") {
-        if (
-          req.user &&
-          (req.user._id.toString() === upload.user ||
-            req.user.role === UserEnum.ADMIN.value ||
-            req.user.role === UserEnum.REVIEWER.value)
-        )
-          return next();
-        return sendError(res, {
+  try {
+    const upload = await FindFileByName(name);
+    if (upload.user !== "public") {
+      if (
+        req.user &&
+        (req.user._id.toString() === upload.user ||
+          req.user.role === UserEnum.ADMIN.value ||
+          req.user.role === UserEnum.REVIEWER.value)
+      )
+        return next();
+      return sendError(
+        res,
+        {
           msg: "You are not authorized to view this file!",
-        });
-      }
-      return next();
-    })
-    .catch(next);
+        },
+        403
+      );
+    }
+    return next();
+  } catch (e) {
+    return next(e);
+  }
 };
