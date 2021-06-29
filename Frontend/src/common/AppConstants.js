61bcf9026260f13953d6acd43c7a88cf11f44483
yasas <manukayasas99@gmail.com>
2021-06-29 21:10:05 +0530

    url up

--- Frontend/src/common/AppConstants.js
+++ Frontend/src/common/AppConstants.js
@@ -1,3 +1,3 @@
-export const APP_BASE_URL = "http://conference-management-sliit.herokuapp.com";
+export const APP_BASE_URL = "https://conference-management-sliit.herokuapp.com";
 export const APP_USER_TOKEN = "conference_tool_user_token";
 export const PAYHERE_MERCHANT = "1214373";
